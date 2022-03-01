4ef9be3af89b8497f83382826593badfe74d34aa
eyalkapi <eyalkapi@gmail.com>
2022-03-01 16:01:53 +0200

    Add stuff

--- app/frontend/src/components/Search.js
+++ app/frontend/src/components/Search.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import '../css/Search.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
@@ -8,7 +9,15 @@ import AdvancedSearch from './AdvancedSearch';
 import Loading from './Loading';
 
 function Search() {
+  useEffect(async () => {
+    const sentenceFileNames = await axios.get('/files/sentences');
+    setSentenceFileNames(sentenceFileNames.data.fileNames);
+  }, [])
+
   const [advanced, setAdvanced] = useState(false);
+  const [sentenceFileNames, setSentenceFileNames] = useState([]);
+  const [fileSelection, setFileSelection] = ('')
+  const [sentenceList, setSentenceList] = useState([]);
 
   const toggleAdvanced = () => {
     setAdvanced(!advanced)
@@ -16,12 +25,33 @@ function Search() {
 
   const search = (event) => {
     event.preventDefault();
+
+    // Eyal's sentence loader goes here before Dror's search
+  }
+
+  const changeFileSelection = (event) => {
+    setFileSelection(event.target.value);
   }
 
   return (
     <div className="mt-3">
         <form>
             <p class="display-3 text-center">חיפוש</p>
+            <div class="form-group col-6 mb-3">
+              <label for="sentenceFileSelect">בחר טקסט לחיפוש משפטים: </label>
+              <select class="form-control" id="sentenceFileSelect" onChange={changeFileSelection}>
+                {
+                  sentenceFileNames.map((fileName) => {
+                    const data = fileName.replace(/\.[^/.]+$/, "").split('-');
+                    const text = data[0];
+                    const lexicon = data[1];
+                    const offset = data[2];
+
+                    return(<option value={fileName}>הטקסט {text} עם הלקסיקון {lexicon} בדילוג {offset} אותיות</option>);
+                  })
+                }
+              </select>
+            </div>
             <div class="accordion" id="accordion">
             <BasicSearch></BasicSearch>
             <AdvancedSearch></AdvancedSearch>
