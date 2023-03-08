fdf2820b48a5c46e39ac687543b1458388f1b6d5
Max Rogol <98maxon98@gmail.com>
2023-03-08 20:51:35 +0200

    algorithm corrected and tested on bereshit36

--- app/api/logic/classes/secret_text.py
+++ app/api/logic/classes/secret_text.py
@@ -1,75 +1,100 @@
+import re
+
 class secret_text(): 
-	'''
-	helper class used to process text into N-skip strings
-	'''
-	def __init__(self, path, letter_offset=2):
-		file_extension = path.split('.')[1]
-		# "uploaded_texts/{path}" path is appended to the cwd the script is run from
-		self.text = getattr(self, f"read_{file_extension}")(f'uploaded-texts/{path}')
-		self.clean_text()
-		self.divide_by_letter_offset(letter_offset)
-		self.name = path.split(".")[0]
+    '''
+    helper class used to process text into N-skip strings
+    '''
+    def __init__(self, path, letter_offset=2):
+        file_extension = path.split('.')[1]
+        # "uploaded_texts/{path}" path is appended to the cwd the script is run from
+        self.text = getattr(self, f"read_{file_extension}")(f'uploaded-texts/{path}')
+        self.clean_text()
+        self.divide_by_letter_offset(letter_offset)
+        self.name = path.split(".")[0]
 
-	#file readers
-	def read_docx(self, file):
-		from docx import Document
-		return " ".join([para.text for para in Document(file).paragraphs])
+        print("DEBUG MARKER")
+        print(self.text)
+        print("DEBUG MARKER")
+#       i = 0
+#       for char in self.text:
+#           print(char,end="")
+#           i += 1
+#           if i == letter_offset:
+#               i = 0
+#               print()
 
-	def read_txt(self, file):
-		with open(file, 'r', encoding="utf-8") as file:
-			return file.read()	
+    #file readers
+    def read_docx(self, file):
+        from docx import Document
+        return " ".join([para.text for para in Document(file).paragraphs])
 
-	def clean_text(self):
-		# clean characters from text, need to add list of characters to choose from
-		chars_to_remove = [',','.','\'',' ','1','2','3','4','5','6','7','8','9','0','\n']
-		for char in chars_to_remove:
-			if char in self.text:
-				self.text = self.text.replace(char, "")
- 
-	def divide_by_letter_offset(self, letter_offset):
-		# prepare text by making N-offset string
-		self.text = "".join([self.text[i::letter_offset] for i in range(letter_offset)])
+    def read_txt(self, file):
+        with open(file, 'r', encoding="utf-8") as file:
+            return file.read()  
 
-	# seems like an unused helper function
-	def check_length(self, second_file, second_file_type):
-		torah_length = 447850
+    def clean_text(self):
+        # clean characters from text, need to add list of characters to choose from
+        # chars_to_remove = [',','.','\'',' ','1','2','3','4','5','6','7','8','9','0','\n']
 
-		if len(self.text) > torah_length:
-			self.text = self.text[:torah_length]
-		else:
-			second_text = secret_text(second_file, second_file_type, letter_offset = self.letter_offset)
-			self.text = self.text + second_file.text
-			self.text = self.text[:torah_length]
 
-	def translate(self, eng_to_heb=True):
-		letter_dict = {
-			'א':   '(',
-			'ב':   'B',
-			'ג':   'G',
-			'ד':   'D',
-			'ה':   'H',
-			'ו':   'W',
-			'ז':   'Z',
-			'ח':   'X',
-			'ט':   '+',
-			'י':   'Y',
-			'כ':   'K',
-			'ל':   'L',
-			'מ':   'M',
-			'נ':   'N',
-			'ס':  'S',
-			'ע':   ')',
-			'פ':  'P',
-			'צ':   'C',
-			'ק':   'Q',
-			'ר':   'R',
-			'ש':  '$',
-			'ת':   'T',
-			}
+        filter_regexp = r'[^קראטוןםפשדגכעיחלךףזסבהנמצתץ]'
+        sofit_translation = {
+                'ך':    'כ',
+                'ם':    'מ',
+                'ן':    'נ',
+                'ף':    'פ',
+                'ץ':    'צ',
+                }
 
-		if eng_to_heb:
-			for k,v in letter_dict.items():
-				self.text = self.text.replace(v,k)
-		else:
-			for k,v in letter_dict.items():
-				self.text = self.text.replace(k,v)
+        self.text = re.sub(filter_regexp, '', self.text)
+        for sofit, normal in sofit_translation.items():
+            self.text = re.sub(sofit, normal, self.text)
+
+
+    def divide_by_letter_offset(self, letter_offset):
+        # prepare text by making N-offset string
+        self.text = "".join([self.text[i::letter_offset] for i in range(letter_offset)])
+
+    # seems like an unused helper function
+    def check_length(self, second_file, second_file_type):
+        torah_length = 447850
+
+        if len(self.text) > torah_length:
+            self.text = self.text[:torah_length]
+        else:
+            second_text = secret_text(second_file, second_file_type, letter_offset = self.letter_offset)
+            self.text = self.text + second_file.text
+            self.text = self.text[:torah_length]
+
+    def translate(self, eng_to_heb=True):
+        letter_dict = {
+            'א':   '(',
+            'ב':   'B',
+            'ג':   'G',
+            'ד':   'D',
+            'ה':   'H',
+            'ו':   'W',
+            'ז':   'Z',
+            'ח':   'X',
+            'ט':   '+',
+            'י':   'Y',
+            'כ':   'K',
+            'ל':   'L',
+            'מ':   'M',
+            'נ':   'N',
+            'ס':  'S',
+            'ע':   ')',
+            'פ':  'P',
+            'צ':   'C',
+            'ק':   'Q',
+            'ר':   'R',
+            'ש':  '$',
+            'ת':   'T',
+            }
+
+        if eng_to_heb:
+            for k,v in letter_dict.items():
+                self.text = self.text.replace(v,k)
+        else:
+            for k,v in letter_dict.items():
+                self.text = self.text.replace(k,v)
--- app/api/logic/run_algorithm.py
+++ app/api/logic/run_algorithm.py
@@ -8,61 +8,61 @@ import json
 import os
 
 def run_algorithm(text, lexicon, letter_offset=2, from_stage=0, save_results=True):
-	'''
-	execute the main algorithm.
-	text is the text to find sentences from.
-	lexicon is the lexicon from which to find words in the text
-	from_stage is the stage from which the program is about to start:
-		- stage 0 is from scratch
-		- stage 1 is after run_find_all_with_MP has been run, with its output used for the next and last stage
-		- save_results indicates whether the generated sentence trees should be saved into a file
-	'''
+    '''
+    execute the main algorithm.
+    text is the text to find sentences from.
+    lexicon is the lexicon from which to find words in the text
+    from_stage is the stage from which the program is about to start:
+        - stage 0 is from scratch
+        - stage 1 is after run_find_all_with_MP has been run, with its output used for the next and last stage
+        - save_results indicates whether the generated sentence trees should be saved into a file
+    '''
 
-	# "output/found-word-indices/{text.name}-{lexicon.name}.txt" is appended to the cwd the script is run from
-	found_word_path = f'output/found-word-indices/{text.name}-{lexicon.name}-{letter_offset}.txt'
+    # "output/found-word-indices/{text.name}-{lexicon.name}.txt" is appended to the cwd the script is run from
+    found_word_path = f'output/found-word-indices/{text.name}-{lexicon.name}-{letter_offset}.txt'
 
-	if from_stage == 0:
-		os.makedirs(os.path.dirname(found_word_path), exist_ok=True)
+    if from_stage == 0:
+        os.makedirs(os.path.dirname(found_word_path), exist_ok=True)
 
-		# run word searching algorithm	
-		indices_dict = run_find_all_MP(lexicon.words, text.text)
+        # run word searching algorithm  
+        indices_dict = run_find_all_MP(lexicon.words, text.text)
 
-		with open(found_word_path, 'w', encoding='utf8') as output_file:
-			output_file.write(json.dumps(indices_dict, ensure_ascii=False))
-	elif from_stage == 1:
-		with open(found_word_path, 'r', encoding='utf8') as input_file:
-			indices_dict = json.loads(input_file.read())
-		
-	# intermediate stage. The lexicon with word indices is converted to a list,
-	# where each index holds the lengths of every word that starts from the corresponding index in the text
-	word_lengths = [[] for _ in range(len(text.text))]
+        with open(found_word_path, 'w', encoding='utf8') as output_file:
+            output_file.write(json.dumps(indices_dict, ensure_ascii=False))
+    elif from_stage == 1:
+        with open(found_word_path, 'r', encoding='utf8') as input_file:
+            indices_dict = json.loads(input_file.read())
 
-	for word, indices in indices_dict.items():
-	    for index in indices:
-	        word_lengths[index].append(len(word))
+    # intermediate stage. The lexicon with word indices is converted to a list,
+    # where each index holds the lengths of every word that starts from the corresponding index in the text
+    word_lengths = [[] for _ in range(len(text.text))]
 
-	#running sentence extraction from index list 
-	all_sentence_trees = run_get_sentence_trees_MP(word_lengths, text.text)
+    for word, indices in indices_dict.items():
+        for index in indices:
+            word_lengths[index].append(len(word))
 
-	if save_results:
-		# append all sentences to one long list and remove all sentences with less than two words
-		all_sentences = list(filter(lambda sentence: len(sentence.split(' ')) > 2, reduce(lambda a,b: a + b, map(lambda tree: tree.get_sentences(), all_sentence_trees), [])))
+    #running sentence extraction from index list 
+    all_sentence_trees = run_get_sentence_trees_MP(word_lengths, text.text)
 
-		save_sentences_path = f'output/sentences/{text.name}-{lexicon.name}-{letter_offset}.txt'
-		os.makedirs(os.path.dirname(save_sentences_path), exist_ok=True)
+    if save_results:
+        # append all sentences to one long list and remove all sentences with less than two words
+        all_sentences = list(filter(lambda sentence: len(sentence.split(' ')) > 2, reduce(lambda a,b: a + b, map(lambda tree: tree.get_sentences(), all_sentence_trees), [])))
 
-		with open(save_sentences_path, 'w', encoding='utf8') as save_file:
-			save_file.write(json.dumps(all_sentences, ensure_ascii=False))
+        save_sentences_path = f'output/sentences/{text.name}-{lexicon.name}-{letter_offset}.txt'
+        os.makedirs(os.path.dirname(save_sentences_path), exist_ok=True)
 
-	return all_sentences
+        with open(save_sentences_path, 'w', encoding='utf8') as save_file:
+            save_file.write(json.dumps(all_sentences, ensure_ascii=False))
+
+    return all_sentences
 
 # usage example
 if __name__ == "__main__":
-	# open text and lexicon
-	het = secret_text("het_and_punish.txt")
-	lexicon = lexicon("dictionary2.txt")
+    # open text and lexicon
+    het = secret_text("het_and_punish.txt")
+    lexicon = lexicon("dictionary2.txt")
 
-	# restrict lexicon word lengths
-	lexicon.set_word_limit(min_len=3, max_len=12)
+    # restrict lexicon word lengths
+    lexicon.set_word_limit(min_len=3, max_len=12)
 
-	run_algorithm(het, lexicon, from_stage=1)
+    run_algorithm(het, lexicon, from_stage=1)
