3c8a51576cca1d479876888de448e12081125d6a
Max Rogol <98maxon98@gmail.com>
2023-05-31 21:23:01 +0300

    Frontend is kinda up

--- vm_sandbox/start_vm.sh
+++ vm_sandbox/start_vm.sh
@@ -2,8 +2,10 @@
 
 qemu-system-x86_64 -enable-kvm -m 1G -cpu host -smp 1 \
     -usb -device usb-tablet -drive file=sandbox.raw,format=raw \
-    -nic user,hostfwd=tcp::60022-:22 \
-    -nic user,hostfwd=tcp::60080-:80 \
-    -nic user,hostfwd=tcp::60443-:443 \
-    -nic user,hostfwd=tcp::60990-:9090 \
-    -nographic
+    -nic tap \
+#   -nic user,hostfwd=tcp::60022-:22 \
+#   -nic user,hostfwd=tcp::60080-:80 \
+#   -nic user,hostfwd=tcp::60443-:443 \
+#   -nic user,hostfwd=tcp::61443-:6443 \
+#   -nic user,hostfwd=tcp::60990-:9090 \
+#   -nographic
