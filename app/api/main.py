14f0d2ec452415e2190daea05e88e524da80c504
Max Rogol <98maxon98@gmail.com>
2023-06-13 22:44:57 +0300

    Prepare to a4fab8c

--- app/api/main.py
+++ app/api/main.py
@@ -1,6 +1,8 @@
 from api import api
 
 # update paths
-api.config['LEXICONS_FOLDER'] = '/home/eyalkapi/Documents/wordsearch-project/uploaded-lexicons'
-api.config['TEXTS_FOLDER'] = '/home/eyalkapi/Documents/wordsearch-project/uploaded-texts'
+# api.config['LEXICONS_FOLDER'] = '/home/eyalkapi/Documents/wordsearch-project/uploaded-lexicons'
+# api.config['TEXTS_FOLDER'] = '/home/eyalkapi/Documents/wordsearch-project/uploaded-texts'
+api.config['LEXICONS_FOLDER'] = '/var/wordsearch/uploaded-lexicons'
+api.config['TEXTS_FOLDER'] = '/var/wordsearch/uploaded-texts'
 api.run()
